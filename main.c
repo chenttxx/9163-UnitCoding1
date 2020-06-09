2a2e7bc286548eeee3f9daf669790fbb5d7b4006
Constantine Kleomenis Christakos <dean@alum.mit.edu>
2020-06-09 19:27:30 -0400

    updates

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+    printf("hello world!");
     return 0;
 }
--- spell.c
+++ spell.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "dictionary.h"
 
 int spell(void) {
 
     return 0;
+}
+
+int check_words(FILE *fp, hashmap_t hashtable[], char *misspelled[]) {
+
+	return 0;
+}
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
+
+	return false;
+}
+
+bool check_word(const char* word, hashmap_t hashtable[]) {
+
+	return false;
+
 }
 
